957db1f9bfea62d62b5f50306e75af0a5042b0d6
sami <admin@seattledevicerepair.com>
2023-04-20 10:39:49 -0700

    acccept changes

--- src/utilits/BackendLink.js
+++ src/utilits/BackendLink.js
@@ -1,7 +1,4 @@
 export const BackendLink = "http://127.0.0.1:8000"
-<<<<<<< HEAD
 export const BackendLinkChat = "127.0.0.1:8000"
 // export const BackendLink = "https://web-production-8324.up.railway.app"
 // export const BackendLinkChat = "web-production-8324.up.railway.app"
-=======
->>>>>>> 816aff75914354dbef2c75fa5e7f6dcc76339e4a
